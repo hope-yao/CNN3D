6548f6bdbdb476f4809cc5f9bd28684eaaf20c03
hope-yao <hope-yao@asu.edu>
2016-12-07 16:15:17 -0700

    located where the problem

--- RAM_blocks/temp_test.py
+++ RAM_blocks/temp_test.py
@@ -14,7 +14,7 @@ theano.config.floatX = 'float32'
 floatX = theano.config.floatX
 
 class FeedbackRNN(BaseRecurrent):
-    def __init__(self, **kwargs):
+    def __init__(self, n_itr, bs, input_len, **kwargs):
         super(FeedbackRNN, self).__init__(**kwargs)
         inits = {
             # 'weights_init': IsotropicGaussian(0.01),
@@ -25,30 +25,55 @@ class FeedbackRNN(BaseRecurrent):
         self.mlp = MLP(activations=[Identity()], dims=[13, 2], name="mlp", **inits)
         self.children = [self.mlp]
 
+        self.n_itr = n_itr
+        self.bs = bs
+        self.input_len = input_len
+
     def get_dim(self, name):
         if name == 'b':
-            return 1
+            return 3
         if name == 'c':
-            return 1
+            return 3
         if name == 'inputs':
-            return 1
+            return 3
         # if name == 'dummy':
         #     return 1
 
-    @recurrent(sequences=['dummy'], contexts=['inputs'],states=['b'],outputs=['c'])
-    def apply(self, inputs, dummy, b=tensor.ones((1,2))):
-        inputs = T.concatenate([inputs, b], axis=1)
-        c = self.mlp.apply(inputs)
-        b = c
-        return c
+    @recurrent(sequences=['dummy'], contexts=['inputs'],states=['b'],outputs=['c','test1','test2'])
+    def apply(self, inputs, dummy, b=None):
+        if b == None:
+            b = tensor.ones((self.bs,2)) # n_itr repeated as the first dimension automatically
+            b = b * 2
+        test1 =inputs
+        test2 = b
+        # test2 = T.concatenate([inputs, b], axis=2) #WHY  ValueError: Join argument "axis" is out of range (given input dimensions)
+        # c = self.mlp.apply(inputs)
+        c = inputs
+        return c,test1,test2
 
+
+a = tensor.ones((4,3 , 13))
+b = tensor.ones((4,3 , 2))
+c = T.concatenate([a,b],axis=2)
+f = theano.function([a,b], [c])
+a_ = numpy.ones((4,3 , 13),dtype=floatX)
+b_ = numpy.ones((4,3 , 2),dtype=floatX)
+c_ = f(a_,b_) ##WHY INT64??
+print(numpy.asarray(a_).shape)
+print(numpy.asarray(c_).shape)
+
+n_itr = 4
+bs = 3
+input_len = 11
 x = tensor.lmatrix('x') # batch * input_dim
-feedback = FeedbackRNN()
+feedback = FeedbackRNN(n_itr,bs,input_len)
 feedback.initialize()
 
-u = theano.tensor.zeros(4) #(self.n_iter, batch_size, 1)
-b = feedback.apply(inputs=x, dummy=u)
-f = theano.function([x], [b])
+u = theano.tensor.zeros(n_itr,dtype=floatX) #(self.n_iter, batch_size, 1)
+c,test1,test2 = feedback.apply(inputs=x, dummy=u)
+f = theano.function([x], [c,test1,test2])
 
-l = f(numpy.ones((1,11),dtype='int64'))
-print(l)
+# c,inputs = f(numpy.ones((bs ,input_len),dtype='int64'))
+c,test1,test2= f(numpy.ones((bs ,input_len),dtype='int64')) ##WHY INT64??
+print(c)
+print(test1.shape,test2.shape)

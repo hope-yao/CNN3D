7635d6e1ae697a8b935db413b65a013558fcf677
hope-yao <hope-yao@asu.edu>
2017-03-09 11:45:12 -0700

    Added visualization of LeNet filters and activations

--- Viz_LeNet_tf.py
+++ Viz_LeNet_tf.py
@@ -158,27 +158,29 @@ def main(_):
   # The variables below hold all the trainable weights. They are passed an
   # initial value which will be assigned when we call:
   # {tf.global_variables_initializer().run()}
-  conv1_weights = tf.Variable(
-      tf.truncated_normal([5, 5, NUM_CHANNELS, 32],  # 5x5 filter, depth 32.
-                          stddev=0.1,
-                          seed=SEED, dtype=data_type()))
-  conv1_biases = tf.Variable(tf.zeros([32], dtype=data_type()))
-  conv2_weights = tf.Variable(tf.truncated_normal(
-      [5, 5, 32, 64], stddev=0.1,
-      seed=SEED, dtype=data_type()))
-  conv2_biases = tf.Variable(tf.constant(0.1, shape=[64], dtype=data_type()))
-  fc1_weights = tf.Variable(  # fully connected, depth 512.
-      tf.truncated_normal([IMAGE_SIZE // 4 * IMAGE_SIZE // 4 * 64, 512],
-                          stddev=0.1,
-                          seed=SEED,
-                          dtype=data_type()))
-  fc1_biases = tf.Variable(tf.constant(0.1, shape=[512], dtype=data_type()))
-  fc2_weights = tf.Variable(tf.truncated_normal([512, NUM_LABELS],
-                                                stddev=0.1,
-                                                seed=SEED,
-                                                dtype=data_type()))
-  fc2_biases = tf.Variable(tf.constant(
-      0.1, shape=[NUM_LABELS], dtype=data_type()))
+  with tf.variable_scope("conv"):
+      conv1_weights = tf.Variable(
+          tf.truncated_normal([5, 5, NUM_CHANNELS, 32],  # 5x5 filter, depth 32.
+                              stddev=0.1,
+                              seed=SEED, dtype=data_type()))
+      conv1_biases = tf.Variable(tf.zeros([32], dtype=data_type()))
+      conv2_weights = tf.Variable(tf.truncated_normal(
+          [5, 5, 32, 64], stddev=0.1,
+          seed=SEED, dtype=data_type()))
+      conv2_biases = tf.Variable(tf.constant(0.1, shape=[64], dtype=data_type()))
+  with tf.variable_scope("fc"):
+      fc1_weights = tf.Variable(  # fully connected, depth 512.
+          tf.truncated_normal([IMAGE_SIZE // 4 * IMAGE_SIZE // 4 * 64, 512],
+                              stddev=0.1,
+                              seed=SEED,
+                              dtype=data_type()))
+      fc1_biases = tf.Variable(tf.constant(0.1, shape=[512], dtype=data_type()))
+      fc2_weights = tf.Variable(tf.truncated_normal([512, NUM_LABELS],
+                                                    stddev=0.1,
+                                                    seed=SEED,
+                                                    dtype=data_type()))
+      fc2_biases = tf.Variable(tf.constant(
+          0.1, shape=[NUM_LABELS], dtype=data_type()))
 
   # We will replicate the model structure for the training subgraph, as well
   # as the evaluation subgraphs, while sharing the trainable parameters.
@@ -311,6 +313,9 @@ def main(_):
         print('Validation error: %.1f%%' % error_rate(
             eval_in_batches(validation_data, sess), validation_labels))
         sys.stdout.flush()
+
+    saver = tf.train.Saver()
+    fn = saver.save(sess,'LeNet.ckpt')
     # Finally print the result!
     test_error = error_rate(eval_in_batches(test_data, sess), test_labels)
     print('Test error: %.1f%%' % test_error)
